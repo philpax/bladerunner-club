7d1e564452f3db4a2620f84377927231dde1437a
bryan newbold <bnewbold@robocracy.org>
2024-10-22 12:26:50 -0700

    add label helpers

---
+++ cmd/automod/helpers.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	comatproto "github.com/bluesky-social/indigo/api/atproto"
+	toolsozone "github.com/bluesky-social/indigo/api/ozone"
+	"github.com/bluesky-social/indigo/atproto/syntax"
+	"github.com/bluesky-social/indigo/automod"
+)
+
+func addAccountLabel(c *automod.RecordContext, did syntax.DID, label string) error {
+	am := c.GetAccountMeta(did)
+	eng := c.InternalEngine()
+	if eng.OzoneClient == nil {
+		c.Logger.Warn("skipping label addition", "did", did, "label", label)
+		return nil
+	}
+
+	// check if label is already applied
+	for _, l := range am.AccountLabels {
+		if l == label {
+			return nil
+		}
+	}
+
+	// send label via engine
+	comment := "auto-adding label"
+	_, err := toolsozone.ModerationEmitEvent(c.Ctx, eng.OzoneClient, &toolsozone.ModerationEmitEvent_Input{
+		CreatedBy: eng.OzoneClient.Auth.Did,
+		Event: &toolsozone.ModerationEmitEvent_Input_Event{
+			ModerationDefs_ModEventLabel: &toolsozone.ModerationDefs_ModEventLabel{
+				CreateLabelVals: []string{label},
+				NegateLabelVals: []string{},
+				Comment:         &comment,
+			},
+		},
+		Subject: &toolsozone.ModerationEmitEvent_Input_Subject{
+			AdminDefs_RepoRef: &comatproto.AdminDefs_RepoRef{
+				Did: did.String(),
+			},
+		},
+	})
+	return err
+}
+
+func removeAccountLabel(c *automod.RecordContext, did syntax.DID, label string) error {
+	am := c.GetAccountMeta(did)
+	eng := c.InternalEngine()
+	if eng.OzoneClient == nil {
+		c.Logger.Warn("skipping label removal", "did", did, "label", label)
+		return nil
+	}
+
+	// check if label is already applied
+	exists := false
+	for _, l := range am.AccountLabels {
+		if l == label {
+			exists = true
+			break
+		}
+	}
+	if !exists {
+		return nil
+	}
+
+	// send label via engine
+	comment := "auto-removing label"
+	_, err := toolsozone.ModerationEmitEvent(c.Ctx, eng.OzoneClient, &toolsozone.ModerationEmitEvent_Input{
+		CreatedBy: eng.OzoneClient.Auth.Did,
+		Event: &toolsozone.ModerationEmitEvent_Input_Event{
+			ModerationDefs_ModEventLabel: &toolsozone.ModerationDefs_ModEventLabel{
+				CreateLabelVals: []string{},
+				NegateLabelVals: []string{label},
+				Comment:         &comment,
+			},
+		},
+		Subject: &toolsozone.ModerationEmitEvent_Input_Subject{
+			AdminDefs_RepoRef: &comatproto.AdminDefs_RepoRef{
+				Did: did.String(),
+			},
+		},
+	})
+	return err
+}
